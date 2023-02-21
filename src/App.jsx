84a8c75c0fb6a29b7059ec58ba315be909c300a0
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-02-21 01:40:53 -0500

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import reactLogo from './assets/react.svg'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,9 @@
+import { BiWorld } from 'react-icons/bi';
+
 export default function Navbar() {
-    
+    return (
+        <nav>
+            <p><span><BiWorld /></span>MyTravelJournal</p>
+        </nav>
+    )
 }
