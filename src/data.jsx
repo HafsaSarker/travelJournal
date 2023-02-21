a1772d5e0bbe5fc3a5508c7c124d5f1fa1476374
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-02-21 01:14:28 -0500

    mod files

---
+++ src/data.jsx
@@ -0,0 +1,17 @@
+export default [
+    {
+        title: "Kyoto",
+    
+        location: "Japan",
+
+        moreInfo: "https://handluggageonly.co.uk/2017/06/26/14-things-see-visiting-kyoto-japan/",
+
+        startDate: "12 Jan, 2021",
+
+        endDate: "24 Jan, 2021",
+
+        description: "Kyoto, once the capital of Japan, is a city on the island of Honshu. It's famous for its numerous classical Buddhist temples, as well as gardens, imperial palaces, Shinto shrines and traditional wooden houses. It’s also known for formal traditions such as kaiseki dining, consisting of multiple courses of precise dishes, and geisha, female entertainers often found in the Gion district.",
+
+        imageUrl: "kyoto.jpg"
+    }
+]
